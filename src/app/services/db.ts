2d529816cde8b90d996839315c1f4c993638494a
eytan414 <eytanivri@gmail.com>
2025-06-11 20:55:22 +0300

    create db service for use dexie of indexedDb - wip

--- src/app/services/db.ts
+++ src/app/services/db.ts
@@ -1,37 +1,94 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import Dexie, { Table } from 'dexie';
+import { AppService } from './app-service';
+
+@Injectable({ providedIn: 'root' })
+export class DBService {
+  private readonly appService = inject(AppService);
+
+
+  async addNewCategory(category: Category) {
+    await db.categories.add(category);
+  }
+
+
+  async updateRecordValueByDateAndCategory(newRecord: CategoryRecord) {
+
+  }
+  async updateRecord(newRecord: CategoryRecord) {
+    await db.records.put(newRecord);
+    this.refetchUpdatedRecordsByDate(this.appService.selectedDate());
+  }
+
+  async refetchUpdatedRecordsByDate(date: string) {
+    const chosenDateRecords = await db.records
+      .where("date")
+      .equals(date)
+      .toArray();
+
+    this.appService.selectedDateRecord.set(chosenDateRecords);
+  }
+  
+  async fetchRecordsByDate(date: string) {
+    let categories = this.appService.categories();
+    const chosenDateRecords = await db.records
+      .where("date")
+      .equals(date)
+      .toArray();
+
+    const missingCategories: Category[] = categories.filter(
+      catagory => !chosenDateRecords.some(record => record.categoryId === catagory.id)
+    );
+
+    if (!missingCategories.length) {
+      this.appService.selectedDateRecord.set(chosenDateRecords);
+      return;
+    }
+    const missingRecords: CategoryRecord[] = missingCategories.map((cat: Category) => ({
+      date,
+      categoryId: cat.id!,
+      value: 0,
+    }));
+
+    await db.records.bulkAdd(missingRecords);
+    const updatedResult = await db.records
+      .where('date')
+      .equals(date)
+      .toArray();
+
+    this.appService.selectedDateRecord.set(updatedResult);
+  }
+
+  async getCategories() {
+    const categories = await db.categories.toArray();
+    this.appService.categories.set(categories);
+    return categories;
+  }
+
+}
+
 
 export interface Category {
   id?: number;
-  createdAt?: Date;
+  weight: number;
   name: string;
-  value: number;
-  //consider adding weight
 }
-
+export interface CategoryRecord {
+  id?: number;
+  date: string;
+  categoryId: number;
+  value: number;
+}
 class DB extends Dexie {
   categories!: Table<Category, number>;
+  records!: Table<CategoryRecord, number>;
 
   constructor() {
     super('Database');
     this.version(1).stores({
-      categories: '++id, name, value',
+      records: '++id, categoryId, value, date',
+      categories: '++id, name, weight',
     });
   }
 }
-
 const db = new DB();
-
-@Injectable({ providedIn: 'root' })
-export class DBService {
-  //switch to signals
-  //add get partial for specific date
-  async addCategory(category: Category): Promise<number> {
-    const { name, value } = category;
-    return await db.categories.add({ name, value, createdAt: new Date() });
-  }
-
-  async getCategory(): Promise<Category[]> {
-    return await db.categories.toArray();
-  }
-}
