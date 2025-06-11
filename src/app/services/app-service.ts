11597e0f6a4658197b6e8d4cb887f69262cb4fce
eytan414 <eytanivri@gmail.com>
2025-06-12 00:22:20 +0300

    add option for localized TZ

--- src/app/services/app-service.ts
+++ src/app/services/app-service.ts
@@ -5,8 +5,14 @@ import { Category, CategoryRecord } from './db';
   providedIn: 'root'
 })
 export class AppService {
+  // readonly today = this.getPersonalLocalTZ().toISOString().split('T')[0];
   readonly today = new Date().toISOString().split('T')[0];
   selectedDate = signal<string>(this.today);
   selectedDateRecords = signal<CategoryRecord[]>([]);
   categories = signal<Category[]>([]);
+
+  private getPersonalLocalTZ(): Date {
+    const ms3Hours = 3 * 60 * 60 * 1000;
+    return new Date(new Date().getTime() + ms3Hours);
+  }
 }
