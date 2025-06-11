f2c7fbbfe32507c873d0b6e7e3d46ee362b7be88
eytan414 <eytanivri@gmail.com>
2025-06-12 01:11:09 +0300

    add reusable change date component for 1 day fw\back

---
+++ src/app/components/change-date/change-date.ts
@@ -0,0 +1,26 @@
+import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { AppService } from '../../services/app-service';
+
+@Component({
+  selector: 'change-date',
+  imports: [],
+  templateUrl: './change-date.html',
+  styleUrl: './change-date.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChangeDate {
+  private readonly appService = inject(AppService);
+  direction = input.required<string>();
+
+  changeDate() {
+    const currentSelectedDate = this.appService.selectedDate();
+    const date = new Date(currentSelectedDate);
+    const delta = this.direction() === 'back' ? -1 : 1;
+
+    const newDate = new Date(date);
+    newDate.setDate(date.getDate() + delta);
+    
+    this.appService.selectedDate.set(newDate.toISOString().split('T')[0]);
+  }
+}
