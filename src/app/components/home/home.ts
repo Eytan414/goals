30bae3a884306fc1baa3ca2369c98a73beb71674
eytan414 <eytanivri@gmail.com>
2025-06-11 20:58:16 +0300

    initial component with date reactivity

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,21 +1,49 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import { DBService, Category } from '../../services/db';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, inject, OnInit, signal } from '@angular/core';
+import { DBService, CategoryRecord, Category } from '../../services/db';
+import { AsyncPipe, DatePipe, JsonPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppService } from '../../services/app-service';
 
 @Component({
   selector: 'app-home',
   imports: [
-    AsyncPipe,
+    DatePipe,
     JsonPipe,
+    FormsModule
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {
-  db = inject(DBService);
+export class Home implements OnInit {
+  private readonly db = inject(DBService);
+  readonly appService = inject(AppService);
+  dateChangeEffect = effect(async () => {
+    const date = this.appService.selectedDate();
+    return await this.db.fetchRecordsByDate(date);
+  })
 
-  data$ = this.db.getCategory();
+  async ngOnInit() {
+    await this.db.getCategories();
+  }
 
+  onDateChange(selectedDate: Event) {
+    const date = new Date(String(selectedDate)).toISOString().split('T')[0];
+    this.appService.selectedDate.set(date);
+  }
+
+  decrement(c: CategoryRecord) {
+    const newValue = {
+      ...c,
+      value: c.value - 1
+    }
+    this.db.updateRecord(newValue);
+  }
+  increment(c: Category) {
+    const categoryId = c.id!;
+    // categoryId
+    // this.displayedDate()
+    // this.db.updateRecord(newValue);
+  }
 
 }
