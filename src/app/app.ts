9af704b69a09dd29a5184de6f19fe89b7ebeff95
eytan414 <eytanivri@gmail.com>
2025-06-12 08:21:34 +0300

    forgot to make app.comp standalone+push, also updated page title and removed redundancy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true,
+  changeDetection:ChangeDetectionStrategy.OnPush
 })
-export class App {
-  protected title = 'goals';
-}
+export class App { }
