a76db88bfebea83a2b66f60d5ab10e8553f7b251
eytan414 <eytanivri@gmail.com>
2025-06-14 03:56:53 +0300

    add cateogry extracted to diff component

---
+++ src/app/features/manage-catagories/category-add-form/category-add-form.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, inject, input, signal } from '@angular/core';
+import { ControlContainer, FormsModule, NgForm } from '@angular/forms';
+import { Category, DBService } from '../../../services/db';
+
+@Component({
+  selector: 'category-add-form',
+  imports: [
+    FormsModule,
+  ],
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm }],
+  templateUrl: './category-add-form.html',
+  styleUrl: './category-add-form.scss',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CategoryAddForm {
+  private readonly db = inject(DBService);
+  addNewCategoryForm = input.required<NgForm>();
+
+  newCategoryName = signal<string>('');
+  newCategoryWeight = signal<number>(NaN);
+
+  addNewCategory(addNewCategoryForm: NgForm) {
+    const newCategory: Category = {
+      name: this.newCategoryName(),
+      weight: this.newCategoryWeight()
+    };
+    this.db.addNewCategory(newCategory);
+    //TODO: check why form not reset
+    addNewCategoryForm.reset();
+  }
+
+}
