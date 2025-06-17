a367d33a77a0587b96a3cb8a51ba92b0966cbe70
eytan414 <eytanivri@gmail.com>
2025-06-17 07:14:10 +0300

    auto switch date when day passes, upgraded to es2024 and ts 5.8.3

--- src/app/home/date-selection/countdown/countdown.ts
+++ src/app/home/date-selection/countdown/countdown.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { interval, map } from 'rxjs';
+import { AppService } from '../../../services/app-service';
 
 @Component({
   selector: 'countdown',
@@ -11,6 +12,8 @@ import { interval, map } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Countdown {
+  private readonly appService = inject(AppService);
+
   readonly countdown$ = interval(ONE_SECOND).pipe(
     map(() => {
       const now = new Date();
@@ -24,8 +27,11 @@ export class Countdown {
       const hours = Math.floor(msLeft / ONE_HOUR);
       const minutes = Math.floor((msLeft % ONE_HOUR) / ONE_MINUTE);
       const seconds = Math.floor((msLeft % ONE_MINUTE) / ONE_SECOND);
-
-      return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+      const result = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+      if(result === '23:59:59'){//auto change date on day end
+        this.appService.selectedDate.set(new Date().toISOString().split('T')[0]);
+      }
+      return result;
     })
   );
 }
